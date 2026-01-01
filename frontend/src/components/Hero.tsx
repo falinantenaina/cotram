793b48fff2c5f285ac734f60fd3bf527d0d8a86c
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2026-01-01 16:24:12 +0300

    Add travelFinder

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,12 +1,13 @@
 import { Calendar, Check } from "lucide-react";
 import hero from "../assets/hero.webp";
+import { TravelFinder } from "./TravelFinder";
 import { Button } from "./ui/Button";
 import { Container } from "./ui/Container";
 
 export const Hero = () => {
   return (
-    <div className="bg-dark-gray text-white/80">
-      <Container className="py-12 md:py-16 lg:py-20 xl:py-24">
+    <div className="bg-dark-gray text-white/80 relative sm:pb-16 py-12 md:py-16 lg:py-20 xl:py-24">
+      <Container>
         <div className="grid grid-cols-1 md:grid-cols-2 space-x-4 lg:space-x-12">
           <div className="space-y-4 md:space-y-8">
             <div className="text-primary bg-primary/10 rounded-full py-1 px-3 text-sm w-max">
@@ -34,7 +35,7 @@ export const Hero = () => {
               </Button>
               <Button variant="secondary">Voir les tarifs</Button>
             </div>
-            <ul className="flex md:space-x-4">
+            <ul className="flex space-x-4">
               <li className="flex items-center space-x-2">
                 <div className="border border-primary rounded-full flex items-center justify-center p-1 size-4 text-primary">
                   <Check className="size-4" />
@@ -67,6 +68,7 @@ export const Hero = () => {
           </div>
         </div>
       </Container>
+      <TravelFinder />
     </div>
   );
 };
---
+++ frontend/src/components/TravelFinder.tsx
@@ -0,0 +1,80 @@
+import { Calendar, MapPin } from "lucide-react";
+import { Container } from "./ui/Container";
+
+export const TravelFinder = () => {
+  const today = new Date().toISOString().split("T")[0];
+
+  console.log(today);
+
+  return (
+    <Container className="max-sm-hidden absolute bottom-0 w-max max-w-full translate-y-1/2 left-1/2 -translate-x-1/2">
+      <div className="bg-white text-dark-gray px-4 py-4 md:py-2 lg:px-8 max-w-7xl mx-auto rounded shadow-2xl">
+        <div className="space-y-4 flex gap-x-2 lg:gap-x-6">
+          {/* Departure */}
+          <div className="space-y-2">
+            <label htmlFor="departure" className="uppercase">
+              Départ
+            </label>
+            <div className="relative ">
+              <MapPin
+                className="absolute left-2 top-1/2 -translate-y-1/2 text-dark-gray size-5"
+                strokeWidth={1}
+              />
+              <input
+                type="text"
+                name="departure"
+                id="departure"
+                placeholder="Antananarivo"
+                className="w-full rounded bg-gray-200 py-2 px-8 focus:outline-none focus:ring-1 focus:ring-primary"
+                required
+              />
+            </div>
+          </div>
+
+          {/* Destination */}
+          <div className="space-y-2">
+            <label htmlFor="destination" className="uppercase">
+              Destination
+            </label>
+            <div className="relative ">
+              <MapPin
+                className="absolute left-2 top-1/2 -translate-y-1/2 text-dark-gray size-5"
+                strokeWidth={1}
+              />
+              <input
+                type="text"
+                name="destination"
+                id="destination"
+                placeholder="Antsirabe"
+                className="w-full rounded bg-gray-200 py-2 px-8 focus:outline-none focus:ring-1 focus:ring-primary"
+                required
+              />
+            </div>
+          </div>
+          {/* Date */}
+          <div className="space-y-2">
+            <label htmlFor="date" className="uppercase">
+              Date
+            </label>
+            <div className="relative ">
+              <Calendar
+                className="absolute left-2 top-1/2 -translate-y-1/2 text-dark-gray size-5"
+                strokeWidth={1}
+              />
+              <input
+                type="date"
+                name="destination"
+                min={today}
+                defaultValue={today}
+                id="destination"
+                placeholder="Antsirabe"
+                className="w-full rounded bg-gray-200 py-2 px-8 focus:outline-none focus:ring-1 focus:ring-primary"
+                required
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
+};
