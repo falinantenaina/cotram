2691bd6b0bb2c8aff39cbeb50da6a48c4282cda1
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2026-01-08 21:03:27 +0300

    Add type

---
+++ backend/src/types/index.ts
@@ -0,0 +1,38 @@
+import type { Request } from "express";
+import type { Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  role: "user" | "admin";
+  createdAt: Date;
+}
+
+export interface IRoute extends Document {
+  departure: string;
+  date: Date;
+  time: string;
+  vehicle: string;
+  totalSeats: number;
+  availableSeats: number;
+  occupiedSeats: number[];
+  price: number;
+  status: "scheduled" | "in_progress" | "completed" | "candelled";
+}
+
+export interface IReservation extends Document {
+  user: string;
+  schedule: string;
+  seats: number[];
+  status: "pending" | "confirmed" | "cancelled" | "completed";
+  paymentStatus: "pending" | "paid" | "refunded";
+  bookinReference: string;
+  createdAt: Date;
+  expriresAt: Date;
+}
+
+export interface AuthRequest extends Request {
+  user?: IUser;
+}
