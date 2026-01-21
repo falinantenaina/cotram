0c9c49bc5265694d10ff4396cc82b6c947e0ca77
falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2026-01-21 21:29:48 +0300

    Update server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,36 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import session from "express-session";
 import { connectDB } from "./config/database.js";
 
 import cookieParser from "cookie-parser";
+import passport from "passport";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 500;
+import "./config/passport.js";
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 (async () => {
