489ffae55f5f05b386c45611b146a1f084778180
Falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2026-06-09 14:14:45 +0300

    Import query client from lib

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import { queryClient } from "./lib/queryClient";
 import "./index.css";
-
-const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
