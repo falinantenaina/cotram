4017664ebae200b9c529db1c134c0c37d7c9a86b
Falinantenaina1 <nantenainaranaivojaona96@gmail.com>
2026-06-09 14:10:21 +0300

    Optimize cache & bundle with rollup option

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,16 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+          query: ["@tanstack/react-query"],
+          stores: ["zustand"],
+          ui: ["lucide-react"],
+        },
+      },
+    },
+  },
 });
